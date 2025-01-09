736dba39d195b2db1bb0375fbafe464ab4fd868f
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-09 13:16:46 +0530

    Added Distance Converter program

---
+++ Level1/DistanceConverter.java
@@ -0,0 +1,13 @@
+public class DistanceConverter{
+    public static void main(String[] args){
+        // Create  a double variable and assigned 10.8 to it
+        double distanceInKM = 10.8;
+        // Created a variable to convert the Kilometer to Miles
+        double distanceInMiles = distanceInKM * 1.6;
+
+        // Display the result
+        System.out.println("The distance "+ distanceInKM +" km in miles is "+distanceInMiles);
+
+    }
+}
+
