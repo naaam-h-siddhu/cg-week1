e89bb514837d80fd827784f4282b4dd7b1873e85
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-09 12:54:11 +0530

    Added birthYear and AverageMarks program

---
+++ Level1/AverageMark.java
@@ -0,0 +1,10 @@
+public class AverageMark{
+    public static void main(String[] args){
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+        double averageMarks = (mathsMarks + physicsMarks + chemistryMarks)/ 3;
+        System.out.println("Sam's average mark in PCM is "+averageMarks);
+    }
+}
+
