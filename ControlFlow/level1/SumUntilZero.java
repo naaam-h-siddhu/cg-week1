cc1ec835b95033272df4818b808f9d2260020724
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-10 10:51:31 +0530

    Added the sum until zero and sum until zero or negative programs

---
+++ ControlFlow/level1/SumUntilZero.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SumUntilZero{
+    public static void main(String[] args){
+        // Created a Scanner to take input
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        System.out.println("Enter the number");
+        // Take the input from user
+        int num = sc.nextInt();
+        while(num != 0){
+            sum += num;
+            System.out.println("Enter the number");
+            int temp = sc.nextInt();
+            num = temp;
+        }
+        System.out.print("The sum of number entered is"+sum);
+
+    }
+}
+
