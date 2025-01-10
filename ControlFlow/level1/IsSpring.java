e90123bf640dee977357ee84a427bf2b57b46867
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-10 12:57:34 +0530

    Changed the IsSpring code to take command line input

--- ControlFlow/level1/IsSpring.java
+++ ControlFlow/level1/IsSpring.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class IsSpring{
     public static void main(String[] args){
-        // Created a Scanner to take input
-        Scanner sc = new Scanner(System.in);
 
         // Take the input of month and date
-        int month = sc.nextInt();
-        int day = sc.nextInt();
+        int month = Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
+
         // March = 3 June = 6
         boolean flag  = false;
         if(month == 3 && day >=20)
