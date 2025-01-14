0b8ef71da507c8480788ae1355d037da19656508
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-14 10:59:37 +0530

    Added the Longest word program

---
+++ JavaString/EXTRAS-StringHandling/LongestWord.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LongestWord {
+    public static String getLongestWord(String s){
+        String [] words = s.split(" ");
+        int longest = 0;
+        for (int i = 0; i < words.length; i++) {
+            longest = Math.max(words[i].length(),longest);
+
+        }
+        for(String it :words){
+            if(it.length() == longest){
+                return it;
+
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        // Created a scanner for inputs
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence");
+        String s = sc.nextLine();
+        String  ans = getLongestWord(s);
+        System.out.println("The longes word is "+ans);
+    }
+}
